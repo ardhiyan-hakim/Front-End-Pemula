3e3c2b1aa6e487d7f7bcf25c35147f11a3ca2b61
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-06-12 04:23:56 +0700

    Parting Render Data Into Another Function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,20 +2,22 @@ import createComponent from "./component.js";
 
 const books = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-  const submitBtn = document.querySelector('#submit');
+document.addEventListener("DOMContentLoaded", () => {
+  const submitBtn = document.querySelector("#submit");
 
-  submitBtn.addEventListener('click', (e) => {
+  submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const id = +new Date();
-    const title = document.querySelector('#book-title').value;
-    const author = document.querySelector('#author').value;
-    const publicationYear = document.querySelector('#publication-year').value;
-    const isComplete = document.querySelector('#is-read').checked;  
+    const title = document.querySelector("#book-title").value;
+    const author = document.querySelector("#author").value;
+    const publicationYear = document.querySelector("#publication-year").value;
+    const isComplete = document.querySelector("#is-read").checked;
 
-    createComponent(title, author, publicationYear, isComplete);
+    putData(id, title, author, publicationYear, isComplete);
+    renderData();
   });
+
 });
 
 function putData(id, title, author, publicationYear, isComplete) {
@@ -25,7 +27,25 @@ function putData(id, title, author, publicationYear, isComplete) {
     author,
     year: publicationYear,
     isComplete,
-  }
+  };
 
   books.push(data);
+  console.log(books);
+}
+
+function renderData() {
+  const readContainer = document.querySelector('#read-container');
+  const unreadContainer = document.querySelector('#unread-container');
+
+  readContainer.innerHTML = '';
+  unreadContainer.innerHTML = '';
+
+  books.forEach((data) => {
+    createComponent(
+      data.title,
+      data.author,
+      data.year,
+      data.isComplete
+    );
+  });
 }
