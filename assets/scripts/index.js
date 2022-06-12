24aa06d647457fbf09a898fdf2b9274c0d496dca
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-06-12 21:05:42 +0700

    Fixing Method To Check, Undo and Remove Item

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,129 +3,168 @@ const books = [];
 document.addEventListener("DOMContentLoaded", () => {
   const submitBtn = document.querySelector("#submit");
   submitBtn.addEventListener("click", (e) => {
-    submitBook(e, books);
+    e.preventDefault();
+
+    insertBookToLibrary();
   });
 });
 
 // SUBMIT ACTION
-function submitBook(e, books) {
-  e.preventDefault();
-
-  const id = +new Date();
-  const title = document.querySelector("#book-title").value;
-  const author = document.querySelector("#author").value;
-  const publicationYear = document.querySelector("#publication-year").value;
-  const isComplete = document.querySelector("#is-read").checked;
-
-  putData(id, title, author, publicationYear, isComplete, books);
+function insertBookToLibrary() {
+  createData();
   renderData(books);
 }
 
-function putData(id, title, author, publicationYear, isComplete, books) {
-  const data = {
-    id,
-    title,
-    author,
-    year: publicationYear,
-    isComplete,
-  };
+function createData() {
+  const id = `Book-${+new Date()}`;
+  const title = document.querySelector("#book-title").value;
+  const author = document.querySelector("#author").value;
+  const year = document.querySelector("#publication-year").value;
+  const isCompleted = document.querySelector("#is-read").checked;
 
-  books.push(data);
+  const currBook = createObject(id, title, author, year, isCompleted);
+  books.push(currBook);
+}
+
+function createObject(id, title, author, year, isCompleted) {
+  return { id, title, author, year, isCompleted };
 }
 
 function renderData(books) {
-  const readContainer = document.querySelector('#read-container');
-  const unreadContainer = document.querySelector('#unread-container');
+  const readContainer = document.querySelector("#read-container");
+  const unreadContainer = document.querySelector("#unread-container");
 
-  readContainer.innerHTML = '';
-  unreadContainer.innerHTML = '';
+  readContainer.innerHTML = "";
+  unreadContainer.innerHTML = "";
 
-  books.forEach((data) => {
-    createComponent(
-      data.id,
-      data.title,
-      data.author,
-      data.year,
-      data.isComplete,
-    );
+  books.forEach((book) => {
+    createComponent(book);
   });
 }
 
-// COMPONENT CREATOR 
-function createComponent(id, title, author, publicationYear, isComplete, books) {
-  const cardTitle = document.createElement('h2');
-  const cardDetail = document.createElement('h4');
-  const trashBtn = document.createElement('span');
+// COMPONENT CREATOR
+function createComponent(book) {
+  const cardTitle = document.createElement("h2");
+  const cardDetail = document.createElement("h4");
+  const trashBtn = document.createElement("span");
 
-  cardTitle.innerText = title;
-  cardDetail.innerText = `${author} - ${publicationYear}`;
-  
-  trashBtn.classList.add('material-symbols-outlined');
-  trashBtn.innerText = ' delete ';
+  cardTitle.innerText = book.title;
+  cardDetail.innerText = `${book.author} - ${book.year}`;
 
-  trashBtn.addEventListener('click', () => {
-    removeBookFromLibrary(id, books);
-  })
-  
-  if (isComplete) {
-    const undoBtn = document.createElement('span');
-    undoBtn.classList.add('material-symbols-outlined');
-    undoBtn.innerText = ' undo ';
+  trashBtn.classList.add("material-symbols-outlined");
+  trashBtn.innerText = " delete ";
 
-    undoBtn.addEventListener('click', () => {
-      moveBookToReadBooks(id, books);
-    })
+  trashBtn.addEventListener("click", () => {
+    removeBookFromLibrary(book.id);
+  });
 
-    createCard(id, cardTitle, cardDetail, undoBtn, trashBtn, isComplete);
+  if (book.isCompleted) {
+    const undoBtn = document.createElement("span");
+    undoBtn.classList.add("material-symbols-outlined");
+    undoBtn.innerText = " undo ";
+
+    undoBtn.addEventListener("click", () => {
+      moveBookToUnreadBooks(book.id);
+    });
+
+    createCard(book.id, cardTitle, cardDetail, undoBtn, trashBtn, book.isCompleted);
   } else {
-    const doneBtn = document.createElement('span');
-    doneBtn.classList.add('material-symbols-outlined');
-    doneBtn.innerText = ' done_outline ';
+    const doneBtn = document.createElement("span");
+    doneBtn.classList.add("material-symbols-outlined");
+    doneBtn.innerText = " done_outline ";
 
-    doneBtn.addEventListener('click', () => {
-      moveBookToUnreadBooks(id, books);
-    })
+    doneBtn.addEventListener("click", () => {
+      moveBookToReadBooks(book.id);
+    });
 
-    createCard(id, cardTitle, cardDetail, doneBtn, trashBtn, isComplete);
+    createCard(book.id, cardTitle, cardDetail, doneBtn, trashBtn, book.isCompleted);
   }
 }
 
-function createCard(id, cardTitle, cardDetail, optBtn, trashBtn, isComplete) {
-  const cardContent = document.createElement('div');
-  cardContent.classList.add('card__content');
-  const cardBtn = document.createElement('div');
-  cardBtn.classList.add('card__button');
+function createCard(id, cardTitle, cardDetail, optBtn, trashBtn, isCompleted) {
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card__content");
+  const cardBtn = document.createElement("div");
+  cardBtn.classList.add("card__button");
 
   cardContent.append(cardTitle);
   cardContent.append(cardDetail);
   cardBtn.append(optBtn);
   cardBtn.append(trashBtn);
 
-  if (isComplete) {
-    const readCard = document.createElement('div');
-    readCard.classList.add('read__card');
+  if (isCompleted) {
+    const readCard = document.createElement("div");
+    readCard.classList.add("read__card");
 
     readCard.append(cardContent);
     readCard.append(cardBtn);
-    readCard.setAttribute('id', `Book-${id}`);
-    inputCardToDOM(readCard, isComplete)
+    readCard.setAttribute("id", `${id}`);
+    inputCardToDOM(readCard, isCompleted);
   } else {
-    const unreadCard = document.createElement('div');
-    unreadCard.classList.add('unread__card');
+    const unreadCard = document.createElement("div");
+    unreadCard.classList.add("unread__card");
 
     unreadCard.append(cardContent);
     unreadCard.append(cardBtn);
-    unreadCard.setAttribute('id', `Book-${id}`);
-    inputCardToDOM(unreadCard, isComplete)
+    unreadCard.setAttribute("id", `Book-${id}`);
+    inputCardToDOM(unreadCard, isCompleted);
   }
 }
 
-function inputCardToDOM(card, isComplete) {
-  if (isComplete) {
-    const readContainer = document.querySelector('#read-container');
-    readContainer.prepend(card);
+function inputCardToDOM(card, isCompleted) {
+  console.log(books);
+
+  if (isCompleted) {
+    const readContainer = document.querySelector("#read-container");
+    readContainer.append(card);
   } else {
-    const unreadContainer = document.querySelector('#unread-container');
-    unreadContainer.prepend(card);
+    const unreadContainer = document.querySelector("#unread-container");
+    unreadContainer.append(card);
   }
+}
+
+function moveBookToUnreadBooks(id) {
+  const currBook = findBook(id);  
+  if (currBook == null) return;
+
+  currBook.isCompleted = false;
+  renderData(books);
+}
+
+function moveBookToReadBooks(id) {
+  const currBook = findBook(id);
+  if (currBook == null) return;
+
+  currBook.isCompleted = true;
+  renderData(books);
+}
+
+function removeBookFromLibrary(id) {
+  console.log(`${id}-Trash Button`);
+
+  const currBook = findIndexBook(id);
+  if (currBook === -1) return;
+
+  books.splice(currBook, 1);
+  renderData(books);
+}
+
+function findBook(id) {
+  for (const book of books) {
+    if (book.id === id) {
+      return book;
+    }
+  }
+
+  return null;
+}
+
+function findIndexBook(id) {
+  for (const bookIndex in books) {
+    if (books[bookIndex].id === id) {
+      return bookIndex;
+    }
+  }
+
+  return -1;
 }
